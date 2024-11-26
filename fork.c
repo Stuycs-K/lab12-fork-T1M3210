03d45e7b74c27555cb0e080f35efcf8153801b23
Tim <timngstuy@gmail.com>
2024-11-26 18:35:35 -0500

    should be good now

--- fork.c
+++ fork.c
@@ -4,14 +4,16 @@
 #include <time.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <string.h>
 
 int randomint() {
 	int id = getpid();
-	return id % 6;
+	return id % 5 + 1;
 }
 
 int main() {
 	printf("%d about to create %d child processes\n", getpid(), 2);
+	int stat;
 	pid_t p, q;
 	(p = fork()) && (q = fork());
 	//if (p < 0) perror("p fork fail");
@@ -22,14 +24,25 @@ int main() {
 		printf("%d %dsec\n", getpid(), prand);
 		sleep(prand);
 		printf("%d finished after %dsec\n", getpid(), prand);
+		return prand;
 	} else if (q == 0) {
 		// child q code
 		int qrand = randomint();
 		printf("%d %dsec\n", getpid(), qrand);
 		sleep(qrand);
 		printf("%d finished after %dsec\n", getpid(), qrand);
+		return qrand;
 	} else {
 		// parent code
+		pid_t r = wait(&stat);
+		if (r < 0) {
+			printf("error: %d\n", errno);
+			printf("%s\n", strerror(errno)); 
+			exit(1);
+		}
+		if (WIFEXITED(stat)) {
+			printf("Main Process %d is done. Child %d slept for %dsec\n", getpid(), r, WEXITSTATUS(stat));
+		}
 	}
 	return 0;
 }
